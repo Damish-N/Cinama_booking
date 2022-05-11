2b2e83f13eda8eeeee6b5fb5f9f84b74502ac756
Damish-N <damishnisal100@gmail.com>
2022-05-11 09:14:19 +0530

    1:commit the initail

---
+++ common/footer.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../cinama/css/footer.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="footer-links">
+    <ul>
+        <li><a href="#">CONTACT US</a></li>
+        <li><a href="#">SUPPORT</a></li>
+        <li><a href="#">ABOUT US</a></li>
+        <li><a href="#">PRIVACY & POLICY</a></li>
+        <li><a href="#">RESEND BOOKING CONFIGURATION</a></li>
+    </ul>
+    <div class="detail-section">
+        <div class="card-payment">
+            <img width="40px" height="40px" src="../../cinama/image/profile.jpg" alt="logo-image">
+            <img width="40px" height="40px" src="../../cinama/image/profile.jpg" alt="logo-image">
+            <img width="40px" height="40px" src="../../cinama/image/profile.jpg" alt="logo-image">
+        </div>
+        <h4 style="color: white">Officially opzz</h4>
+    </div>
+</div>
+</body>
+</html>
---
+++ common/header.php
@@ -0,0 +1,41 @@
+<?php
+//    echo 'hello'
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../cinama/css/header.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="header-area">
+    <div class="image-area">
+        <img width="100px" height="100px"   src="../../cinama/image/download.png" alt="logo-image">
+        <h1 style="text-align: center">QUICK TICKET</h1>
+
+    </div>
+    <div class="profile-area">
+        <img width="40px" height="40px" src="../../cinama/image/profile.jpg" alt="logo-image">
+    </div>
+</div>
+<nav class="nav-bar">
+
+    <ul class="nav-bar-items">
+        <li><a href="../../cinama/home.php">Home</a></li>
+        <li><a href="../../cinama/movies.php">Movies</a></li>
+        <li><a href="#">Theater</a></li>
+        <li><a href="#">My Tickets</a></li>
+        <li><a href="#">Notification</a></li>
+    </ul>
+    <div class="search-bar">
+        <input type="text" placeholder="search">
+    </div>
+</nav>
+</body>
+</html>
+

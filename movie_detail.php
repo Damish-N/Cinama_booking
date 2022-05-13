1bd7d2b20e02951418e3073651b708c07ba5655f
Damish-N <damishnisal100@gmail.com>
2022-05-13 23:46:44 +0530

    3:detail-page

---
+++ movie_detail.php
@@ -0,0 +1,48 @@
+<?php
+include '../cinama/common/header.php'
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/movie_detail.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="detail-container">
+    <div class="static-container">
+        <div class="rate-1">
+            <h2>8.5</h2>
+            <h3>IMDM</h3>
+        </div>
+        <div class="rate-1">
+            <h2>50%</h2>
+            <h3>Rotten Tomatoes</h3>
+        </div>
+        <div class="rate-1">
+            <h2>7.8</h2>
+            <h3>Amazon Rate</h3>
+        </div>
+
+    </div>
+    <div class="cover-image-container">
+        <img src="./image/h7-slide6.jpg" alt="">
+    </div>
+    <div class="more-detail">
+
+    </div>
+</div>
+<div class="btn-area-container">
+
+</div>
+</body>
+</html>
+<?php
+include '../cinama/common/footer.php'
+?>
+
+
+

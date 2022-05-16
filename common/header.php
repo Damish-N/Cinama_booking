3302e9a962880d26a32d3b5dd01741ad9b14fe0f
Damish-N <damishnisal100@gmail.com>
2022-05-16 15:37:21 +0530

    4:add-movie

--- common/header.php
+++ common/header.php
@@ -18,16 +18,13 @@
 <header>
     <div id="nav">
         <ul>
-            <li><a href="../index.php">Home</a></li>
-            <li><a href="servicepage.php">Services</a></li>
+            <li><a href="../../cinama/home.php">Home</a></li>
+            <li><a href="#">Theater</a></li>
             <li><a href="">Category</a>
                 <ul class="dropdown">
-                    <li><a href="catogery\cat1.php">Home & Living</a></li>
-                    <li><a href="catogery\cat2.php">Arts & Crafts</a></li>
-                    <li><a href="catogery\cat3.php">Tools & Home Improvement</a></li>
-                    <li><a href="catogery\cat4.php">Home Gardening</a></li>
-                    <li><a href="catogery\cat5.php">Toys & Educational</a></li>
-                    <li><a href="catogery\cat6.php">Kitchen & Dining</a></li>
+                    <li><a href="/../cinama/movies.php">English Movies</a></li>
+                    <li><a href="/../cinama/movies.php">Tamil Movies</a></li>
+                    <li><a href="/../cinama/movies.php">Sinhala Movies</a></li>
                 </ul>
             </li>
             <li><a href="">About us</a></li>

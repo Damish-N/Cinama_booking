c024061a3ee5cc97ba4661fd6dee1cdda5db4d15
Damish-N <damishnisal100@gmail.com>
2022-05-19 02:45:14 +0530

    6:set the data with film and connect with theater

--- controllers/theatersController.php
+++ controllers/theatersController.php
@@ -4,39 +4,50 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 include "../controllers/addMovieController.php";
 
-function getAllTheaters(){
+function getAllTheaters()
+{
     $query = "SELECT * FROM theater_owner;";
 
     return executeQuery($query);
 
 }
 
-function getTheTheater($id){
+function getTheTheater($id)
+{
     $query = "SELECT * FROM theater_owner WHERE  theater_id='$id'";
     return executeQuery($query);
 }
 
-function getTheMoviesViaTheater($id){
+function getTheMoviesViaTheater($id)
+{
     $query = "SELECT movie_id FROM movie_theater where theater_id = $id";
     return executeQuery($query);
 }
 
-function getTheMovieById($id){
+function getTheMovieById($id)
+{
     $query = "SELECT * FROM movie where movie_id = '$id'";
     return executeQuery($query);
 }
 
-function getAvailableSeats($date,$movie_id,$theater_id,$show_time){
+function getAvailableSeats($date, $movie_id, $theater_id, $show_time)
+{
     $query = "SELECT SUM(no_balcony_seats) as no_balcony_seats ,SUM(no_odc_seats) as no_odc_seats,SUM(no_of_box) as no_of_box
 FROM ticket WHERE show_date='$date' AND movie_id = '$movie_id' AND theater_owner = '$theater_id' and show_time='$show_time';";
     return executeQuery($query);
 }
 
-function getAllTheMoviesTypeVice($type){
-    $query ="SELECT * FROM movie WHERE language = '$type' AND on_screening=1;";
+function getAllTheMoviesTypeVice($type)
+{
+    $query = "SELECT * FROM movie WHERE language = '$type' AND on_screening=1;";
     return executeQuery($query);
 }
 
+function getAllTheatersShowCorrespondingMovie($movie_id)
+{
+    $query = "SELECT theater_id FROM movie_theater WHERE movie_id = '$movie_id';";
+    return executeQuery($query);
+}
 
 
 ?>
--- views/movieDetial.php
+++ views/movieDetial.php
@@ -1,4 +1,14 @@
 <?php
+include '../controllers/theatersController.php';
+$movie_id = $_GET['movie_id'];
+
+$movie = getTheMovieById($movie_id);
+$theaters = getAllTheatersShowCorrespondingMovie($movie_id);
+$theaters_details = Array();
+for ($i=0;$i<sizeof($theaters);$i++){
+    $theaters_details[]=getTheTheater($theaters[$i]['theater_id']);
+}
+
 ?>
 
 <!doctype html>
@@ -11,49 +21,71 @@
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-<!---->
+    <!---->
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/footer.css">
 
     <link rel="stylesheet" href="../css/style.css">
 
     <link rel="stylesheet" href="../css/movieDetail.css">
+    <link rel="stylesheet" href="../css/theater.css">
+
     <title>movie detail</title>
 </head>
 <body>
-    <?php include '../common/header.php'?>
+<?php include '../common/header.php' ?>
 
-    <div class="movie-container">
+<div class="movie-container">
 
-        <div class="image-area">
-            <img src="../image/single-movie.jpg" alt="single-movie">
-        </div>
-        <div class="detail-area">
-            <button class="btn">BOOK A TICKET FOR THIS MOVIE</button>
-            <i class="fa fa-clock-o" aria-hidden="true"><span style="color: red">169 min</span></i>
-            <br>
-            <h5>Country:<span style="font-weight: lighter"> New Zeland, USA</span></h5>
-            <h5>Year:<span style="font-weight: lighter"> 2019</span></h5>
-            <h5>Category:<span style="font-weight: lighter"> Adventure, Fantazy</span></h5>
-            <h5>Release Date:<span style="font-weight: lighter"> December 12, 2012</span></h5>
-            <h5>Box office:<span style="font-weight: lighter"> $1 017 003 568</span></h5>
-            <h5>Director:<span style="font-weight: lighter">  Peter Jackson</span></h5>
-            <h5>Description:<span style="font-weight: lighter">
-                We use cookies to make sure that our website works properly, as well as some ‘optional’ cookies to personalise content and advertising, provide social media features and analyse how people use our site. By accepting some or all optional cookies you give consent to the processing of your personal data.</span></h5>
-
+    <div class="image-area">
+        <?php echo '<img src="' . $movie[0]['url'] . '" alt="single-movie">' ?>
+    </div>
+    <div class="detail-area">
+        <button class="btn">BOOK A TICKET FOR THIS MOVIE</button>
+        <i class="fa fa-clock-o" aria-hidden="true"><span
+                    style="color: red"><?php echo $movie[0]['duration'] ?> min</span></i>
+        <br>
+        <h5>Name:<span style="font-weight: lighter"><?php echo $movie[0]['name'] ?></span></h5>
+        <h5>Country:<span style="font-weight: lighter"> <?php echo $movie[0]['language'] ?></span></h5>
+        <h5>Category:<span style="font-weight: lighter"> <?php echo $movie[0]['language'] ?></span></h5>
+        <h5>Release Date:<span style="font-weight: lighter"><?php echo $movie[0]['release_date'] ?></span></h5>
+        <h5>Votes:<span style="font-weight: lighter"><?php echo $movie[0]['votes'] ?> </span></h5>
+        <h5>Director:<span style="font-weight: lighter"> <?php echo $movie[0]['director_name'] ?></span></h5>
+        <h5>Description:<span style="font-weight: lighter">
+                <?php echo $movie[0]['description'] ?>
             <ul class="tags">
-                <li><a href="#" class="tag">IMDM rate : <span>8.5/10</span></a> </li>
+                <li><a href="#" class="tag">IMDM rate : <span><?php echo $movie[0]['imbm'] ?>/10</span></a> </li>
                 <li><a href="#" class="tag">Amarzon : <span>8/10</span></a></li>
             </ul>
 
             <button class="btn watch-trailer-btn">Watch Trailer</button>
 
+    </div>
+</div>
+
+<div class="theaterCard">
+    <?php if (sizeof($theaters_details) > 0) {
+        for ($i = 0; $i < sizeof($theaters_details); $i++) {
+            echo '
+            <div class="my-card">
+        <div class="card-content">
+            <div class="img-place">
+                <div id="card-img"></div>
+            </div>
+            <div class="card-text">
+                <p>'.$theaters_details[$i][0]['theater_name'].'-Thetaer</p>
+                <a href="../views/dateTimeAllocate.php?movie_id='.$movie_id.'&theater_id='.$theaters[$i]['theater_id'].'" class="btn">Book Now</a>
+            </div>
         </div>
+
     </div>
 
+        ';
+        }
+    } ?>
+</div>
 
-
-    <?php include '../common/footer.php'?>
+<?php include '../common/footer.php' ?>
 
 </body>
 </html>
